7d28035a9ecc969d0e11fa52bd944ac9fee5fc27
thatkawaiiguy <ivanthegreat15@gmail.com>
2017-06-10 11:15:51 -0400

    Do more layouts

--- app/src/main/java/com/bigdj/emergetransaction/TransactionActivity.java
+++ app/src/main/java/com/bigdj/emergetransaction/TransactionActivity.java
@@ -1,16 +1,16 @@
 package com.bigdj.emergetransaction;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by thatkawaiiguy on 6/10/17.
+ */
 
+public class TransactionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
